ac21bb449454d63b096e8e96eaece1d7af5c68e8
Tania Amparan <you@email.com>
2017-01-30 17:29:08 -0800

    exported info from nyt

--- js/main.js
+++ js/main.js
@@ -1,17 +1,44 @@
+$(document).ready(function(){
+ // Aquí va nuestro código para manipular el DOM
+ 
+ $('.select').on('change', function(){
 
+  var $topNews = $('.topNews');
+  $topNews.empty();
+  var $selecting = $('.select').val();
+  //aqui ajax toma nyt info
+  $.ajax({
+    method: 'GET',
+    dataType:'Json',
+    url:'https://api.nytimes.com/svc/topstories/v2/' + $selecting + '.json?api-key=f7c8f05f4bcb4d3d9957fd20748d7c93'
+  })
+  .done(function(response){
+      
+      var $dataResults = response.results.filter(function(item){ 
+        return item.multimedia.length;
+      }).splice(0, 12); 
+      
+      var newInfo ='';
 
-$(function(){
-  
-}
-  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "f7c8f05f4bcb4d3d9957fd20748d7c93"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
+      $.each($dataResults, function(item, value){
+
+          
+          var $abstract =value.abstract;
+          var $images =value.multimedia[4].url;
+          var $url =value.url;
+
+          newInfo += '<li class="picture" style="background-image: url(' + value.multimedia[4].url + ')">';
+          newInfo += '<a href="' + value.url + '"></a>';
+          newInfo +='<p class="abstract">' + value.abstract + '</p>' + '</li>';
+     
+      });
+
+      $topNews.append(newInfo);
+
+    }).always(function(){
+      
+    });
+
+  });
+
 });
